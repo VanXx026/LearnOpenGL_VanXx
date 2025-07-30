ac8bc2b042ee7b2fd525ee42552e1d1ce4d23cbf
VanXx <79959119+VanXx026@users.noreply.github.com>
2025-07-30 21:59:56 +0800

    hello triangle 1

--- OpenGL_VanXx/GetStarted/Main.cpp
+++ OpenGL_VanXx/GetStarted/Main.cpp
@@ -2,9 +2,67 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+//float vertices[] =
+//{
+//	-0.5f, -0.5f, 0.0f,
+//	0.5f, -0.5f, 0.0f,
+//	0.0f, 0.5f, 0.0f
+//};
+float vertices[] =
+{
+	-1.0f, -1.0f, 0.0f,
+	1.0f, -1.0f, 0.0f,
+	0.0f, 1.0f, 0.0f
+};
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
+
 void frame_buffer_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+void check_shader_Fail(GLuint shader, GLenum status)
+{
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		if (status == GL_COMPILE_STATUS)
+		{
+			std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
+		}
+		if (status == GL_LINK_STATUS)
+		{
+			std::cout << "ERROR::SHADER::LINK_FAILED\n" << infoLog << std::endl;
+		}
+	}
 }
 
 int main()
@@ -36,14 +94,72 @@ int main()
 		return -1;
 	}
 
-	glViewport(0, 0, 200, 300);
+	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(window, frame_buffer_callback);
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO); // TODO：为什么这里是用引用，下面不是？
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// 把数据复制到绑定的缓冲区
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	check_shader_Fail(vertexShader, GL_COMPILE_STATUS);
+
+	unsigned int fragShader;
+	fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragShader);
+	check_shader_Fail(fragShader, GL_COMPILE_STATUS);
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragShader);
+	glLinkProgram(shaderProgram);
+	check_shader_Fail(shaderProgram, GL_LINK_STATUS);
+
+	//glUseProgram(shaderProgram);
+	//glDeleteShader(vertexShader);
+	//glDeleteShader(fragShader);
+
+	//// 链接顶点属性
+	//// 0 => layout(location = 0)
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	//glEnableVertexAttribArray(0);
+
+	// 使用VAO
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	// 绑定VAO
+	glBindVertexArray(VAO);
+	// 把顶点数组复制到缓冲区中
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 设置顶点属性指针
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
 	// Render Loop
 	while (!glfwWindowShouldClose(window))
 	{
-		glfwSwapBuffers(window);
+		// 处理输入
+		process_input(window);
+
+		// Render Call
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		// 检查并调用事件，交换缓冲区
 		glfwPollEvents();
+		glfwSwapBuffers(window);
 	}
 
 	glfwTerminate();
